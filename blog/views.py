7765e0d501fba156e2e072723a2dbd9f4b53fbd6
ham <770034226@qq.com>
2021-12-27 16:44:13 +0800

    updata css

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
+
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("欢迎访问我的博客首页！")
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
+    
