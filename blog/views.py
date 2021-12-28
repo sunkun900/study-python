9ed58fbdadb148f67f128c0f829d04dd73079aa8
ham <770034226@qq.com>
2021-12-28 16:27:15 +0800

    updata

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+
 
 
 
@@ -9,3 +12,17 @@ from .models import Post
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        # 记得在顶部引入 TocExtension 和 slugify
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
+
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+
+    return render(request, 'blog/detail.html', context={'post': post})
